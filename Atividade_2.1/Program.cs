7bf43469270432ae00a885802e93cc889638ff98
virtualkami <kamillyvfq@gmail.com>
2022-11-27 21:19:07 -0400

    Adicionar arquivos de projeto.

---
+++ Atividade_2.1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Atividade_2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // criação e recebimento da string numeros
+            string numeros = @"Um Dois Três Quatro";
+            // criação e recebimento da string letras
+            string letras = @"A B C D";
+            // bloco using onde esquerda recebe números e direita recebe letras
+            using(StringReader esquerda=new StringReader(numeros),direita=new StringReader(letras))
+            {
+                // criação da string item
+                string item;
+                // bloco do while
+                do
+                {
+                    //item recebe esquerda
+                    item = esquerda.ReadLine();
+                    // mostra item
+                    Console.WriteLine(item);
+                    // item recebe direita
+                    item = direita.ReadLine();
+                    // mostra item
+                    Console.WriteLine(item);
+                    // para a repetição quando item for nulo
+                } while (item != null);
+            }
+        }
+    }
+}
